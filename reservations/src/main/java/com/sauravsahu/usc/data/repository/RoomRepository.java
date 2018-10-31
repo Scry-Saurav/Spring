ec733416a5d76fca105c7adfb89008d2fda1054f
SauravSahu <saurav.sahu@scryanalytics.com>
2018-10-31 10:36:02 -0700

    initial commit

---
+++ reservations/src/main/java/com/sauravsahu/usc/data/repository/RoomRepository.java
@@ -0,0 +1,10 @@
+package com.sauravsahu.usc.data.repository;
+
+import com.sauravsahu.usc.data.entity.Room;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoomRepository extends CrudRepository<Room, Long>{
+    Room findByNumber(String number);
+}
